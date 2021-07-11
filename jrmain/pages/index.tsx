209e67de5989be1e4145d34e7b29ae35f80dfaba
jackyrusly <jackyrusly@gmail.com>
2021-07-11 16:11:44 +0700

    Add links

--- jrmain/pages/index.tsx
+++ jrmain/pages/index.tsx
@@ -1,4 +1,5 @@
 import dynamic from 'next/dynamic';
+import Link from 'next/link';
 
 const RemoteHeader = (await import('jrlayout/components/header')).default;
 const RemoteFooter = (await import('jrlayout/components/footer')).default;
@@ -22,6 +23,14 @@ const Index = () => {
         <RemoteCard>
           <h2>Card Example</h2>
         </RemoteCard>
+      </div>
+
+      <div>
+        <Link href="/about">About</Link>
+      </div>
+
+      <div>
+        <Link href="/privacy-policy">Privacy Policy</Link>
       </div>
 
       <RemoteFooter />
