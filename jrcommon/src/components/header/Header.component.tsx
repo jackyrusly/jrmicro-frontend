687e57dcb9512ca60e25361d93d6a4cdcb824cb0
jackyrusly <jackyrusly@gmail.com>
2021-07-12 18:18:02 +0700

    Update header

--- jrcommon/src/components/header/Header.component.tsx
+++ jrcommon/src/components/header/Header.component.tsx
@@ -1,5 +1,34 @@
+import { useRouter } from 'next/router';
+import styles from './Header.module.scss';
+
 const Header = () => {
-  return <header>Header</header>;
+  const router = useRouter();
+
+  return (
+    <header className={styles['site-header']}>
+      <div className={styles['site-identity']}>
+        <h1>
+          <div onClick={() => router.push('/')}>JRMicro Frontend</div>
+        </h1>
+      </div>
+
+      <nav className={styles['site-navigation']}>
+        <ul>
+          <li>
+            <div onClick={() => router.push('/')}>Home</div>
+          </li>
+          <li>
+            <div onClick={() => router.push('/about')}>About</div>
+          </li>
+          <li>
+            <div onClick={() => router.push('/privacy-policy')}>
+              Privacy Policy
+            </div>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
